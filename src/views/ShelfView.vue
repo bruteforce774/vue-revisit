fc1f230d25048cf289d1067a63e96dd6e4905861
Daniel Andreas Wang <codingforpeace@protonmail.com>
2026-02-06 23:54:08 +0100

    update ShelfView

--- src/views/ShelfView.vue
+++ src/views/ShelfView.vue
@@ -2,6 +2,8 @@
   import { useBookStore } from '../stores/bookStore'
   import { useRoute } from 'vue-router'
   import { useRouter } from 'vue-router'
+  import { ref } from 'vue'
+  import BookList from '../components/BookList.vue'
 
   const bookStore = useBookStore();
   const router = useRouter();
@@ -10,18 +12,40 @@
   const newAuthor = ref('')
 
   function handleAdd() {
-
+    const title = newTitle.value.trim()
+    const author = newAuthor.value.trim()
+    if(!title || !author) return
+    bookStore.addBook(title, author)
+    newTitle.value = ''
+    newAuthor.value = ''
   }
 
-  function handleRemove() {
-
+  function handleRemove(id: number) {
+    bookStore.removeBook(id)
   }
 
-  function handleSelect() {
-  
+  function handleSelect(id: number) {
+    router.push({ name: 'book-detail', params: { id } })
   }
 </script>
 
-<template></template>
+<template>
+  <div>
+    <h1>My Bookshelf</h1>
+    <p>{{ bookStore.bookCount }} book(s) on the shelf</p>
+
+    <form @submit.prevent="handleAdd">
+      <input v-model="newTitle" placeholder="Title" required />
+      <input v-model="newAuthor" placeholder="Author" required />
+      <button type="submit">Add Book</button>
+    </form>
+
+    <BookList
+      :books="bookStore.books"
+      @remove="handleRemove"
+      @select="handleSelect"
+    />
+  </div>
+</template>
 
 <style></style>
